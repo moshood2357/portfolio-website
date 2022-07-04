3d60cd0b52899bc39dcbe2476c7022518c410d6d
Moshood Sanusi <sanusimoshoodabiola@gmail.com>
2022-07-04 07:43:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const showcase = new Typed(".typing", {
-    strings: ["", "Web Developer", "Ui/Ux Designer", "Mobile App Developer"],
+    strings: ["", "Web Developer", "UI/UX Designer", "Mobile App Developer"],
     typeSpeed: 100,
     BackSpeed: 60,
     loop: true,
